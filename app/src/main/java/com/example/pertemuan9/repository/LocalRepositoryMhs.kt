dc9ac68f6a465c55a67be845a50dfc116aec859b
AryaGGWP <arya.lampung.2016@gmail.com>
2024-12-16 20:30:45 +0700

    8th commit: update LocalRepositoryMhs, memanggil fungsi dari MahasiswaDao

--- app/src/main/java/com/example/pertemuan9/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan9/repository/LocalRepositoryMhs.kt
@@ -2,11 +2,20 @@ package com.example.pertemuan9.repository
 
 import com.example.pertemuan9.data.dao.MahasiswaDao
 import com.example.pertemuan9.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs(
     private val mahasiswaDao: MahasiswaDao
 ) : RepositoryMhs {
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
+
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+
+    fun getMhs(nim: String): Flow<Mahasiswa>
+
+    suspend fun deleteMhs(nim: String): Flow<Mahasiswa>
+
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
     }
 }
