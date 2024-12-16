7dc095fe04ab781b5d101efcbc5409296a2b2b9f
AryaGGWP <arya.lampung.2016@gmail.com>
2024-12-16 20:42:12 +0700

    10th commit: menambahkan file HomeMhsViewModel

---
+++ app/src/main/java/com/example/pertemuan9/ui/viewmodel/HomeMhsViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.pertemuan9.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan9.repository.RepositoryMhs
+
+class HomeMhsViewModel (
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+
+}
