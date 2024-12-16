267c44519c1548b7057e430320d87ad1f8812d58
AryaGGWP <arya.lampung.2016@gmail.com>
2024-12-16 20:59:39 +0700

    14th commit: menambahkan data class DetailUiState pada file DetailMhsViewModel

--- app/src/main/java/com/example/pertemuan9/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan9/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,2 +1,17 @@
 package com.example.pertemuan9.ui.viewmodel
 
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+
+//data class untuk menampung data yang akan ditampilkan di UI
+
