9377e1a2b4e52bd25f2956d473d03caad557c6eb
AryaGGWP <arya.lampung.2016@gmail.com>
2024-12-16 20:22:54 +0700

    6th commit: update RepositoryMhs, menambahkan method getMhs daan deleteMhs

--- app/src/main/java/com/example/pertemuan9/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan9/repository/RepositoryMhs.kt
@@ -7,4 +7,11 @@ interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
 
     fun getAllMhs(): Flow<List<Mahasiswa>>
+    //memanggil fungsi getMahasiswa dari MahasiswaDao
+
+    fun getMhs(nim: String): Flow<Mahasiswa>
+    //memanggil fungsi untuk memanggil data sesuai NIM
+    
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+
 }
