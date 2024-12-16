06e6bdd8d5d1ae6fc2c82ffdc528fac4f61718f0
AryaGGWP <arya.lampung.2016@gmail.com>
2024-12-16 21:02:10 +0700

    15th commit: menambahkan fungsi Mahasiswa.toDetailUiEvent pada file DetailMhsViewModel

--- app/src/main/java/com/example/pertemuan9/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan9/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.pertemuan9.ui.viewmodel
 
+import com.example.pertemuan9.data.entity.Mahasiswa
+
 data class DetailUiState(
     val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
     val isLoading: Boolean = false,
@@ -15,3 +17,14 @@ data class DetailUiState(
 
 //data class untuk menampung data yang akan ditampilkan di UI
 
+//memindahkan data dari entity ke UI
+fun Mahasiswa.toDetailUiEvent(): MahasiswaEvent {
+    return MahasiswaEvent(
+        nim = nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+}
