46bef84fe2aa4eb3a28dcdddc75b5d2e0d8f79a0
AryaGGWP <arya.lampung.2016@gmail.com>
2024-12-16 20:15:25 +0700

    2nd commit: menambahkan query getAllMahasiswa dan getMahasiswa

--- app/src/main/java/com/example/pertemuan9/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/pertemuan9/data/dao/MahasiswaDao.kt
@@ -2,10 +2,23 @@ package com.example.pertemuan9.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.pertemuan9.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MahasiswaDao {
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa(): Flow <List<Mahasiswa>>
+    //mengambil data mahasiswa dari database, urut berdasarkan ascending(ASC)
+
+    @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
+    fun getMahasiswa(nim: String): Flow<Mahasiswa>
+    //mengambil data mahasiswa berdasarkan NIM
+
+
 }
+
