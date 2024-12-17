c5b497a228fa9798216217461e8dae2a7a079be5
AryaGGWP <arya.lampung.2016@gmail.com>
2024-12-17 20:31:22 +0700

    final

--- app/src/main/java/com/example/pertemuan9/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan9/repository/LocalRepositoryMhs.kt
@@ -4,18 +4,27 @@ import com.example.pertemuan9.data.dao.MahasiswaDao
 import com.example.pertemuan9.data.entity.Mahasiswa
 import kotlinx.coroutines.flow.Flow
 
-class LocalRepositoryMhs(
+class LocalRepositoryMhs (
     private val mahasiswaDao: MahasiswaDao
-) : RepositoryMhs {
-    override suspend fun insertMhs(mahasiswa: Mahasiswa) {
+):RepositoryMhs{
+    override suspend fun  insertMhs(mahasiswa: Mahasiswa){
         mahasiswaDao.insertMahasiswa(mahasiswa)
+    }
 
-    fun getAllMhs(): Flow<List<Mahasiswa>>
+    //get all mahasiswa
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
 
-    fun getMhs(nim: String): Flow<Mahasiswa>
+    override fun getMhs(nim: String): Flow<Mahasiswa> {  //mengambil data mahasiswa berdasarkan nim
+        return mahasiswaDao.getMahasiswa(nim)
+    }
 
-    suspend fun deleteMhs(nim: String): Flow<Mahasiswa>
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) { //menghapus data mahasiswa
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
 
-    suspend fun updateMhs(mahasiswa: Mahasiswa)
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) { //memperbarui data mahasiswa dalam database
+        mahasiswaDao.updateMahasiswa(mahasiswa)
     }
 }
